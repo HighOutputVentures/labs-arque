a3ee0bc2c8f21d919faff348dce3170f9d148120
rogermadjos <roger@hov.co>
2022-10-11 11:27:07 +0800

    update store trait

--- apps/store/src/store/store.rs
+++ apps/store/src/store/store.rs
@@ -6,7 +6,12 @@ custom_error!{pub InsertEventError
   InvalidAggregateVersion = "invalid aggregate version"
 }
 
+pub struct ListAggregateEventsParams<'a> {
+  aggregate_id: &'a [u8],
+  aggregate_version: Option<u32>
+}
+
 pub trait Store {
   fn insert_event(event: &Event) -> Result<(), InsertEventError>;
-  fn list_aggregate_events(&self) -> Result<(), Error>; 
+  fn list_aggregate_events(&self, params: ListAggregateEventsParams) -> Result<(), Error>; 
 }
