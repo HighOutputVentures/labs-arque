3f6cdb99b184f6bea5634e4fe7700d7b2ef221f3
Alres Arena <alres.arena2019@gmail.com>
2022-11-26 16:02:02 +0800

    feat: add driver instance validation

--- libs/node-driver/src/arque-driver.ts
+++ libs/node-driver/src/arque-driver.ts
@@ -1,18 +1,16 @@
-
 import { Event } from './types';
 
 const { driverNew, insertEvent } = require('../bin/arque-driver.node');
 export class ArqueDriver {
-
   private driver: any;
 
   constructor(endpoint: string) {
     this.driver = driverNew(endpoint);
-
   }
 
   async insertEvent(event: Event) {
-     return insertEvent(this.driver, event);
-  }
+    if (!this.driver) throw new Error('Invalid driver instance');
 
+    return insertEvent(this.driver, event);
+  }
 }
