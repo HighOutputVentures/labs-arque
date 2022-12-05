754502848b13a828842bba35df90d9f9282bd0e0
Alres Arena <alres.arena2019@gmail.com>
2022-12-05 12:42:18 +0800

    feat: implement server inialitization on store

--- apps/store/src/main.rs
+++ apps/store/src/main.rs
@@ -3,9 +3,10 @@ mod server;
 mod store;
 mod stream;
 
+use server::{Server, ServerConfig};
 use std::error::Error;
 use std::sync::mpsc::channel;
-// use server::Server;
+use tempdir::TempDir;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
@@ -20,6 +21,21 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
 
     //   vec![]
     // }, rx)?;
+ 
+    let server_endpoint = String::from("tcp://*:4000");
+    let server_temp = server_endpoint.clone();
+    let temp_dir = TempDir::new("arque_test").unwrap();
+   
+    let server = Server::new(ServerConfig {
+        data_path: Some(temp_dir.path()),
+    });
+
+    println!("path: {:?}", temp_dir.path());
+    println!("server started @ {:?}", server_temp);
+
+    server.serve(server_endpoint, _rx).unwrap();
+
+  
 
     Ok(())
 }
