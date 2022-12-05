65aa579e6a8e537d15b8bf0ed3a49d172fbb4251
Alres Arena <alres.arena2019@gmail.com>
2022-12-05 12:39:23 +0800

    feat: initial implementation of list aggregate events for node driver

--- libs/driver/src/types.rs
+++ libs/driver/src/types.rs
@@ -1,5 +1,5 @@
-pub struct ListAggregateEventsParams<'a> {
-    pub aggregate_id: &'a [u8],
+pub struct ListAggregateEventsParams {
+    pub aggregate_id: Vec<u8>,
     pub aggregate_version: Option<u32>,
     pub limit: u32,
 }
