4f329e53bebfc201fc4366f9327a16cae7ab553d
Alres Arena <alres.arena2019@gmail.com>
2022-12-07 16:44:12 +0800

    test: add function call assertion

--- libs/node-client/src/aggregate-instance.test.ts
+++ libs/node-client/src/aggregate-instance.test.ts
@@ -16,82 +16,94 @@ describe('AggregateInstance', () => {
         dateTimeCreated: Date;
         dateTimeLastUpdated: Date;
       };
-      
+
       type AccountAggregateState = {
-        root: Account,
+        root: Account;
       };
-      
+
       enum EventType {
         AccountCreated = 0,
         AccountUpdated = 1,
-      };
-      
+      }
+
       type AccountUpdatedEvent = Event<
         EventType.AccountUpdated,
         Partial<Pick<Account, 'password' | 'metadata'>>
-      >;      
+      >;
 
       const id = new ObjectId();
       const password = await hash(faker.internet.password(), 10);
 
-      const ClientMock = {
-        listAggregateEvents: jest.fn().mockResolvedValue([
-          {
-            id: new ObjectId(),
-            aggregate: {
-              id,
-              version: 2,
-            },
-            type: EventType.AccountUpdated,
-            body: {
-              password,
-            },
-            meta: {},
-            timestamp: new Date(),
-          },
-        ]),
+      const event = {
+        id: new ObjectId(),
+        aggregate: {
+          id,
+          version: 2,
+        },
+        type: EventType.AccountUpdated,
+        body: {
+          password,
+        },
+        meta: {},
+        timestamp: new Date(),
       };
 
-      const eventHandlers = [
-        {
-          type: EventType.AccountUpdated,
-          handle(ctx, event: AccountUpdatedEvent) {
-            return {
-              root: {
-                ...ctx.state.root,
-                ...event.body,
-                dateTimeLastUpdated: event.timestamp,
-              },
-            };
-          },
-        },
-      ];
+      const ClientMock = {
+        listAggregateEvents: jest.fn().mockResolvedValue([event]),
+      };
 
-      let aggregate = new AggregateInstance<Command, Event, AccountAggregateState, {}>(
-        id,
-        1,
-        {
-          root: {
-            id,
-            name: faker.name.firstName().toLowerCase(),
-            password: await hash(faker.internet.password(), 10),
-            metadata: {
-              firstName: faker.name.firstName(),
-              lastName: faker.name.lastName(),
+      const eventHandler = {
+        type: EventType.AccountUpdated,
+        handle: jest.fn((ctx, event: AccountUpdatedEvent) => {
+          return {
+            root: {
+              ...ctx.state.root,
+              ...event.body,
+              dateTimeLastUpdated: event.timestamp,
             },
-            dateTimeCreated: new Date(),
-            dateTimeLastUpdated: new Date(),
-          }
+          };
+        }),
+      };
+
+      const eventHandlers = [eventHandler];
+      const version = 1;
+      const state = {
+        root: {
+          id,
+          name: faker.name.firstName().toLowerCase(),
+          password: await hash(faker.internet.password(), 10),
+          metadata: {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+          },
+          dateTimeCreated: new Date(),
+          dateTimeLastUpdated: new Date(),
         },
-        ClientMock as never,
-        [],
-        eventHandlers
-      );
+      };
+
+      let aggregate = new AggregateInstance<
+        Command,
+        Event,
+        AccountAggregateState,
+        {}
+      >(id, version, state, ClientMock as never, [], eventHandlers);
 
       await aggregate.reload();
 
       expect(aggregate.version).toEqual(2);
       expect(aggregate.state.root.password).toEqual(password);
+
+      expect(ClientMock.listAggregateEvents).toBeCalledTimes(1);
+      expect(
+        ClientMock.listAggregateEvents.mock.calls[0][0].aggregate.id
+      ).toEqual(id);
+      expect(
+        ClientMock.listAggregateEvents.mock.calls[0][0].aggregate.version
+      ).toEqual(version);
+
+      expect(eventHandler.handle).toBeCalledTimes(1);
+      expect(eventHandler.handle.mock.calls[0][0].state.root.id).toEqual(id);
+      expect(eventHandler.handle.mock.calls[0][1]).toEqual(event);
     });
 
     // should concurrent
